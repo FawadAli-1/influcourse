2d95680d5c9619bb2bec4538fa24f12d41949411
Fawad Ali <becomebetter101@gmail.com>
2024-07-30 23:42:19 +0500

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Toaster/>
+        <Analytics/>
       </body>
     </html>
   );
